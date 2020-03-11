a976fb33b8fbe28577be719714212af75b59fac4
Oluwaseyi Adeogun <sheyilaaw98@gmail.com>
2020-03-11 21:13:53 +0100

    Add Swagger Doc Spec

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,9 @@
 import {Router} from 'express'
+import * as swaggerUi from 'swagger-ui-express'
+import * as specs from '../swagger'
 
 const routes = Router();
+
+routes.use('/', swaggerUi.serve, swaggerUi.setup(specs.default));
 
 export default routes;
---
+++ src/swagger.ts
@@ -0,0 +1,30 @@
+import * as swaggerJSDoc from 'swagger-jsdoc';
+
+// Swagger set up
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "RestFul API with TypeScript, TypeORM (MySql) and Express",
+      version: "1.0.0",
+      description: "A test project to understand how easy it is to document and Express API",
+      license: {
+        name: 'MIT',
+        url: 'https://opensource.org/licenses/MIT'
+      },
+      contact: {
+        email: "contact@asyncdeveloper.com"
+      }
+    },
+    servers: [
+      {
+        url: "http://localhost:3001/api/v1"
+      }
+    ]
+  },
+  apis: []
+};
+
+const specs = swaggerJSDoc(options);
+
+export default specs
